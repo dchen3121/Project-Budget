8c0157c3af370746a9b4f81a4539e62a1cccef52
dchen <dchen001213@gmail.com>
2019-04-28 21:53:02 -0400

    Initial Commit

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ tag_name = "p"
 query = {"class": "price price--large"}
 
 blazer = Item(url, tag_name, query)
+blazer.save_to_mongo()
 
+# items_loaded = Item.all()
+# print(items_loaded)
+# print(items_loaded[0].load_price())
 
-
-print(blazer.load_price())
+# print(blazer.load_price())
 
--- models/item.py
+++ models/item.py
@@ -4,20 +4,33 @@ from flask import Flask
 from bs4 import BeautifulSoup
 import requests
 import uuid
+from common.database import Database
+
 
 class Item:
     def __init__(self, url: str, tag_name: str, query: Dict, _id: str = None):
+        """
+        Initializes an item object
+        :param url: the url of the thing we are looking to buy
+        :param tag_name: the tag we are looking for (e.g. a 'p' tag)
+        :param query: the query we are searching for for the item, in dictionary format
+        :param _id: the id of the item, may be randomly generated with uuid
+        """
         self.url = url
         self.tag_name = tag_name
         self.query = query
         self.price = None
-        self.collection = "items"
+        self.collection = "pricing"
         self._id = _id or uuid.uuid4().hex
 
     def __repr__(self):
         return f"<item {self.url}>"
 
     def load_price(self) -> float:
+        """
+        Given an item, returns its price in float in $
+        :return: float representing the price of the object in dollars
+        """
         response = requests.get(self.url)
         content = response.content
         soup = BeautifulSoup(content, "html.parser")
@@ -32,6 +45,10 @@ class Item:
         return self.price
 
     def json(self) -> Dict:
+        """
+        Returns a json type representation of the item object
+        :return: a dictionary type containing all the information of the Item
+        """
         return {
             "_id": self._id,
             "url": self.url,
@@ -40,10 +57,8 @@ class Item:
         }
 
     def save_to_mongo(self):
+        """
+        Saves the object to mongoDB
+        :return: void
+        """
         Database.insert(self.collection, self.json())
-
-
-
-
-
-
