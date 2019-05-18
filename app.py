5fa8c262ede3a9eb3254c48c55f84968644f4714
dchen <dchen001213@gmail.com>
2019-05-18 02:33:00 -0400

    Deleted dotenv

--- app.py
+++ app.py
@@ -4,14 +4,12 @@ from views.pricing.stores import store_blueprint
 from views.pricing.alerts import alert_blueprint
 from views.users import user_blueprint
 from views.personal.spendings import spendings_blueprint
-from dotenv import load_dotenv
 
 app = Flask(__name__)
 app.secret_key = "s7@8ob3fn8$%9nf64o&bv02q84gn74v!o78o34u78#27g4fno49lu*7h87q23fb1ui"
 app.config.update(
     ADMIN=os.environ.get('ADMIN')
 )
-load_dotenv()
 
 app.register_blueprint(alert_blueprint, url_prefix="/alerts")  # registering alert_blueprint, setting prefix url
 app.register_blueprint(store_blueprint, url_prefix="/stores")
